2653367188df5d86d30ea36d6db2d7c92b9a8bf0
nuocmambi <nuocmambi@gmail.com>
2016-08-30 18:17:22 +0200

    forste commit

--- playlist/src/main/java/com/tidal/playlist/PlaylistBusinessBean.java
+++ playlist/src/main/java/com/tidal/playlist/PlaylistBusinessBean.java
@@ -16,81 +16,155 @@ import java.util.*;
  */
 public class PlaylistBusinessBean {
 
-    private PlaylistDaoBean playlistDaoBean;
+	private PlaylistDaoBean playlistDaoBean;
+	private final int MAX_TRACK = 500;
 
-    @Inject
-    public PlaylistBusinessBean(PlaylistDaoBean playlistDaoBean){
-        this.playlistDaoBean = playlistDaoBean;
-    }
 
-    List<PlayListTrack> addTracks(String uuid, int userId, List<Track> tracksToAdd, int toIndex,
-                                  Date lastUpdated) throws PlaylistException {
+	@Inject
+	public PlaylistBusinessBean(PlaylistDaoBean playlistDaoBean){
+		this.playlistDaoBean = playlistDaoBean;
+	}
 
-        try {
+	List<PlayListTrack> addTracks(String uuid, int userId, List<Track> tracksToAdd, int toIndex,
+			Date lastUpdated) throws PlaylistException {
 
-            TrackPlayList playList = playlistDaoBean.getPlaylistByUUID(uuid, userId);
+		try {
 
-            //We do not allow > 500 tracks in new playlists
-            if (playList.getNrOfTracks() + tracksToAdd.size() > 500) {
-                throw new PlaylistException("Playlist cannot have more than " + 500 + " tracks");
-            }
+			TrackPlayList playList = playlistDaoBean.getPlaylistByUUID(uuid, userId);
 
-            // The index is out of bounds, put it in the end of the list.
-            if (toIndex > playList.getPlayListTracksSize() || toIndex == -1) {
-                toIndex = playList.getPlayListTracksSize();
-            }
+			//We do not allow > 500 tracks in new playlists
+			if (playList.getNrOfTracks() + tracksToAdd.size() > MAX_TRACK) {
+				throw new PlaylistException("Playlist cannot have more than " + MAX_TRACK + " tracks");
+			}
 
-            if (!validateIndexes(toIndex, playList.getNrOfTracks())) {
-                return Collections.EMPTY_LIST;
-            }
+			// The index is out of bounds, put it in the end of the list.
+			toIndex = checkIndexOutOfBounds(toIndex,playList);
 
-            Set<PlayListTrack> originalSet = playList.getPlayListTracks();
-            List<PlayListTrack> original;
-            if (originalSet == null || originalSet.size() == 0)
-                original = new ArrayList<PlayListTrack>();
-            else
-                original = new ArrayList<PlayListTrack>(originalSet);
+			if (!validateIndex(toIndex, playList.getNrOfTracks())) {
+				return Collections.EMPTY_LIST;
+			}
 
-            Collections.sort(original);
+			// Converts a Set of PlayListTrack to a List of PlayListTrack
+			List<PlayListTrack> original = convertOriginalTrackList(playList.getPlayListTracks());
 
-            List<PlayListTrack> added = new ArrayList<PlayListTrack>(tracksToAdd.size());
+			List<PlayListTrack> added = new ArrayList<PlayListTrack>(tracksToAdd.size());
 
-            for (Track track : tracksToAdd) {
-                PlayListTrack playlistTrack = new PlayListTrack();
-                playlistTrack.setTrack(track);
-                playlistTrack.setTrackPlaylist(playList);
-                playlistTrack.setTrackArtistId(track.getArtistId());
-                playlistTrack.setDateAdded(lastUpdated);
-                playlistTrack.setTrack(track);
-                playList.setDuration(addTrackDurationToPlaylist(playList, track));
-                original.add(toIndex, playlistTrack);
-                added.add(playlistTrack);
-                toIndex++;
-            }
+			for (Track track : tracksToAdd) {
+				//Set values to playlistTrack
+				PlayListTrack playlistTrack = setPlayListTrackValues(track, playList, lastUpdated);
+				playList.setDuration(addTrackDurationToPlaylist(playList, track));
+				original.add(toIndex++, playlistTrack);
+				added.add(playlistTrack);
+			}
 
-            int i = 0;
-            for (PlayListTrack track : original) {
-                track.setIndex(i++);
-            }
+			//set index on tracks
+			original = setIndexOnTracks(original);
 
-            playList.getPlayListTracks().clear();
-            playList.getPlayListTracks().addAll(original);
-            playList.setNrOfTracks(original.size());
+			playList.getPlayListTracks().clear();
+			playList.getPlayListTracks().addAll(original);
+			playList.setNrOfTracks(original.size());
 
-            return added;
+			return added;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new PlaylistException("Generic error");
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new PlaylistException("Generic error");
+		}
+	}
 
-    private boolean validateIndexes(int toIndex, int length) {
-        return toIndex >= 0 && toIndex <= length;
-    }
+	public Track deleteTrack(String uuid, int userId, Track trackToDelete) {
+		try {
+			TrackPlayList playList = playlistDaoBean.getPlaylistByUUID(uuid, userId);
 
-    private float addTrackDurationToPlaylist(TrackPlayList playList, Track track) {
-        return (track != null ? track.getDuration() : 0)
-                + (playList != null && playList.getDuration() != null ? playList.getDuration() : 0);
-    }
+			// Converts a Set of PlayListTrack to a List of PlayListTrack
+			List<PlayListTrack> original = convertOriginalTrackList(playList.getPlayListTracks());
+
+			for (PlayListTrack playListTrack : original) {
+				if (playListTrack.getTrack().equals(trackToDelete)) {
+					Track tmp = playListTrack.getTrack();
+					//removes playlist with the track
+					original.remove(playListTrack);
+					playList.getPlayListTracks().clear();
+					playList.getPlayListTracks().addAll(original);
+					playList.setNrOfTracks(original.size());
+					return tmp;
+				}
+			}
+
+			return null;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new PlaylistException("Generic error");
+		}
+	}
+
+	private boolean validateIndex(int toIndex, int length) {
+		return toIndex >= 0 && toIndex <= length;
+	}
+
+	private float addTrackDurationToPlaylist(TrackPlayList playList, Track track) {
+		return (track != null ? track.getDuration() : 0)
+				+ (playList != null && playList.getDuration() != null ? playList.getDuration() : 0);
+	}
+
+
+	/**
+	 * Check if the index is out of bounds, put it in the end of the list
+	 * @param toIndex
+	 * @param playList
+	 * @return
+	 */
+	public int checkIndexOutOfBounds(int toIndex, TrackPlayList playList) {
+		if (toIndex > playList.getPlayListTracksSize() || toIndex < 0) {
+			return playList.getPlayListTracksSize();
+		}
+		return toIndex;
+	}
+
+	/**
+	 * Converts a Set of PlayListTrack to a sorted List of PlayListTrack
+	 * @param originalSet
+	 * @return
+	 */
+	public List<PlayListTrack> convertOriginalTrackList(Set<PlayListTrack> originalSet) {
+		if (originalSet == null || originalSet.size() == 0) {
+			return new ArrayList<PlayListTrack>();
+		} else {
+			ArrayList<PlayListTrack> newList = new ArrayList<PlayListTrack>(originalSet);
+			Collections.sort(newList);
+			return newList;
+		}
+	}
+
+	/**
+	 * Set method
+	 * set index on tracks
+	 * @param original
+	 * @return
+	 */
+	public List<PlayListTrack> setIndexOnTracks(List<PlayListTrack> original) {
+		int i = 0;
+		for (PlayListTrack track : original) {
+			track.setIndex(i++);
+		}
+		return original;
+	}
+	/**
+	 * Set method
+	 * Sets properties to PlayListTrack
+	 * @param track
+	 * @param playList
+	 * @param lastUpdated
+	 * @return
+	 */
+	public PlayListTrack setPlayListTrackValues(Track track, TrackPlayList playList, Date lastUpdated ) {
+		PlayListTrack playlistTrack = new PlayListTrack();
+		playlistTrack.setTrackPlaylist(playList);
+		playlistTrack.setTrackArtistId(track.getArtistId());
+		playlistTrack.setDateAdded(lastUpdated);
+		playlistTrack.setTrack(track);
+		return playlistTrack;
+	}
+
 }
--- playlist/src/test/java/com/tidal/playlist/PlaylistBusinessBeanTest.java
+++ playlist/src/test/java/com/tidal/playlist/PlaylistBusinessBeanTest.java
@@ -1,19 +1,27 @@
 package com.tidal.playlist;
 
 import com.google.inject.Inject;
+import com.tidal.playlist.dao.PlaylistDaoBean;
 import com.tidal.playlist.data.PlayListTrack;
 import com.tidal.playlist.data.Track;
+import com.tidal.playlist.data.TrackPlayList;
+
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * @author: eivind.hognestad@wimpmusic.com
@@ -23,33 +31,120 @@ import static org.testng.AssertJUnit.assertTrue;
 @Guice(modules = TestBusinessModule.class)
 public class PlaylistBusinessBeanTest {
 
-    @Inject
-    PlaylistBusinessBean playlistBusinessBean;
+	@Inject
+	PlaylistBusinessBean playlistBusinessBean;
 
-    @BeforeMethod
-    public void setUp() throws Exception {
+	List<Track> trackList;
+	Track t1;
+	Track t2;
+	Track t3;
 
-    }
+	@BeforeMethod
+	public void setUp() throws Exception {
+		//Create Tracks
+		t1 = new Track();
+		t1.setArtistId(2);
+		t1.setTitle("Track 2");
+		t1.setTrackNumberIdx(2);
+		t1.setId(10000);
 
-    @AfterMethod
-    public void tearDown() throws Exception {
+		t2 = new Track();
+		t2.setArtistId(3);
+		t2.setTitle("Track 3");
+		t2.setTrackNumberIdx(3);
+		t2.setId(30000);
 
-    }
+		t3 = new Track();
+		t3.setArtistId(5);
+		t3.setTitle("Track 4");
+		t3.setTrackNumberIdx(5);
+		t3.setId(50000);
 
-    @Test
-    public void testAddTracks() throws Exception {
-        List<Track> trackList = new ArrayList<Track>();
+		//create a List with Tracks
+		trackList = new ArrayList<Track>();
+		trackList.add(t1);
+		trackList.add(t2);
+		trackList.add(t3);
 
-        Track track = new Track();
-        track.setArtistId(4);
-        track.setTitle("A brand new track");
-        track.setTrackNumberIdx(1);
-        track.setId(76868);
+	}
 
-        trackList.add(track);
+	@AfterMethod
+	public void tearDown() throws Exception {
 
-        List<PlayListTrack> playListTracks = playlistBusinessBean.addTracks(UUID.randomUUID().toString(), 1, trackList, 5, new Date());
+	}
 
-        assertTrue(playListTracks.size() > 0);
-    }
+	@Test
+	public void testAddTracks() throws Exception {
+		List<PlayListTrack> playListTracks = playlistBusinessBean.addTracks(UUID.randomUUID().toString(), 1, trackList, 5, new Date());
+
+		assertTrue(playListTracks.size() > 0);
+	}
+
+
+	@Test
+	public void testDeleteTracks() throws Exception {
+		Track track = playlistBusinessBean.deleteTrack(UUID.randomUUID().toString(), 1, t1);
+
+		assertTrue((track == null) || (track.equals(t1)));
+	}
+
+	@Test
+	public void testCheckIndexEqualsZero() {
+		TrackPlayList trackPlayList = new TrackPlayList();
+		PlayListTrack playListTrack = new PlayListTrack();
+
+		Set<PlayListTrack> plt = new HashSet<>();
+		plt.add(playListTrack);
+		trackPlayList.setPlayListTracks(plt);
+
+		int returnValue = playlistBusinessBean.checkIndexOutOfBounds(0, trackPlayList);
+
+		assertEquals(returnValue, 0);
+	}
+
+	@Test
+	public void testCheckIndexOverBound() {
+		TrackPlayList trackPlayList = new TrackPlayList();
+		PlayListTrack playListTrack = new PlayListTrack();
+
+		Set<PlayListTrack> plt = new HashSet<>();
+		plt.add(playListTrack);
+		trackPlayList.setPlayListTracks(plt);
+
+		int returnValue = playlistBusinessBean.checkIndexOutOfBounds(2, trackPlayList);
+		assertEquals(returnValue, trackPlayList.getPlayListTracksSize());
+	}
+
+	@Test
+	public void testCheckIndexBelowBound() {
+		TrackPlayList trackPlayList = new TrackPlayList();
+		PlayListTrack playListTrack = new PlayListTrack();
+
+		Set<PlayListTrack> plt = new HashSet<>();
+		plt.add(playListTrack);
+		trackPlayList.setPlayListTracks(plt);
+
+		int returnValue = playlistBusinessBean.checkIndexOutOfBounds(-2, trackPlayList);
+
+		assertEquals(returnValue, trackPlayList.getPlayListTracksSize());
+
+	}
+
+	@Test
+	public void testConvertOriginalTrackList() {
+		PlayListTrack p1 = new PlayListTrack();
+		PlayListTrack p2 = new PlayListTrack();
+		p1.setId(1);
+		p2.setId(2);
+
+		Set<PlayListTrack> set = new HashSet<>();
+		set.add(p1);
+		set.add(p2);
+
+		ArrayList<PlayListTrack> list = (ArrayList<PlayListTrack>) playlistBusinessBean.convertOriginalTrackList(set);
+
+		assertTrue(list.contains(p1));
+		assertTrue(list.contains(p2));
+	}
+
 }
